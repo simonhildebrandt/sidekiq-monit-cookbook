a9dffd428808e970f8714410d722cb58c25e914b
Simon Hildebrandt <simonhildebrandt@gmail.com>
2015-12-14 12:48:58 +1100

    Added logging

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,6 +1,8 @@
 include_recipe "sidekiq-monit::service"
 
 node[:deploy].each do |application, deploy|
+  identifier = "sidekiq_#{application}"
+
   template "#{node[:monit][:conf_dir]}/sidekiq_#{application}.monitrc" do
     mode 0644
     source "sidekiq_monitrc.erb"
@@ -9,7 +11,8 @@ node[:deploy].each do |application, deploy|
       current_path: "#{deploy[:deploy_to]}/current",
       pid_file: "#{deploy[:deploy_to]}/shared/pids/sidekiq.pid",
       rails_env: deploy[:rails_env],
-      identifier: "sidekiq_#{application}"
+      identifier: identifier,
+      log_file: "#{deploy[:deploy_to]}/shared/log/#{identifier}_#{deploy[:rails_env]}.log"
     })
 
     notifies :reload, "service[monit]", :immediately
